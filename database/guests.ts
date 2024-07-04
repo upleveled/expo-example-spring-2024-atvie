fb941732d03c775317e4755af9bbb36c33e9d80d
Lukas Prochazka <prochazka.lu@gmail.com>
2024-07-04 13:49:02 +0200

    Add missing dependencies for prettier

--- database/guests.ts
+++ database/guests.ts
@@ -25,31 +25,31 @@ export const getGuest = async (id: number) => {
 
 export const addGuest = async (newGuest: Omit<Guest, 'id'>) => {
   const [guest] = await sql<Guest[]>`
-      INSERT INTO
-        guests (
-          first_name,
-          last_name,
-          attending
-        )
-      VALUES
-        (
-          ${newGuest.firstName},
-          ${newGuest.lastName},
-          ${newGuest.attending}
-        )
-      RETURNING
-        guests.*
-    `;
+    INSERT INTO
+      guests (
+        first_name,
+        last_name,
+        attending
+      )
+    VALUES
+      (
+        ${newGuest.firstName},
+        ${newGuest.lastName},
+        ${newGuest.attending}
+      )
+    RETURNING
+      guests.*
+  `;
   return guest;
 };
 
 export const deleteGuest = async (removeGuest: Pick<Guest, 'id'>) => {
   const [guest] = await sql<Guest[]>`
-      DELETE FROM guests
-      WHERE
-        id = ${removeGuest.id}
-      RETURNING
-        guests.*
-    `;
+    DELETE FROM guests
+    WHERE
+      id = ${removeGuest.id}
+    RETURNING
+      guests.*
+  `;
   return guest;
 };
