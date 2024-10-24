72839c03f31698205afd65f319255eb9cd51b427
Lukas Prochazka <prochazka.lu@gmail.com>
2024-10-24 20:18:26 +0200

    Add Link

--- components/GuestItem.tsx
+++ components/GuestItem.tsx
@@ -1,7 +1,14 @@
 import { Ionicons } from '@expo/vector-icons';
 import { Image } from 'expo-image';
-import { router } from 'expo-router';
-import { StyleSheet, Switch, Text, TouchableOpacity, View } from 'react-native';
+import { Link } from 'expo-router';
+import {
+  Pressable,
+  StyleSheet,
+  Switch,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import placeholder from '../assets/candidate-default.avif';
 import { colors } from '../constants/colors';
 import type { Guest } from '../migrations/00000-createTableGuests';
@@ -66,78 +73,79 @@ type Props = {
 export default function GuestItem({ guest, guests, setGuests }: Props) {
   const { id, firstName, lastName, attending } = guest;
 
-  const openGuest = () => {
-    router.push({
-      pathname: `/guests/[guestId]`,
-      params: { guestId: id },
-    });
-  };
-
   return (
-    <TouchableOpacity style={styles.container} onPress={openGuest}>
-      <View style={styles.card}>
-        <View style={styles.avatar}>
-          <Image
-            style={styles.avatarImage}
-            source={{
-              uri: `https://res.cloudinary.com/trueque-image/image/upload/v1713269496/guest-${id}.webp`,
-            }}
-            placeholder={placeholder}
-            placeholderContentFit="cover"
-          />
-        </View>
-        <View style={styles.info}>
-          <Text style={styles.name} numberOfLines={1} ellipsizeMode="tail">
-            {firstName} {lastName}
-          </Text>
-          <Text style={styles.attending}>
-            {attending ? 'Attending' : 'Not Attending'}
-          </Text>
-        </View>
-        <View style={styles.actionWrapper}>
-          <Switch
-            value={attending}
-            onValueChange={async () => {
-              const response = await fetch(`/api/${id}`, {
-                method: 'PUT',
-                body: JSON.stringify({
-                  firstName,
-                  lastName,
-                  attending: !attending,
-                }),
-              });
-
-              if (response.ok) {
-                setGuests(
-                  guests.map((g) =>
-                    g.id === id ? { ...g, attending: !attending } : g,
-                  ),
-                );
-              }
-            }}
-            trackColor={{ false: colors.textSecondary, true: colors.switch }}
-            thumbColor={colors.text}
-          />
-          <TouchableOpacity
-            style={styles.button}
-            onPress={async () => {
-              const response = await fetch(`/api/${id}`, {
-                method: 'DELETE',
-              });
-
-              if (response.ok) {
-                setGuests(guests.filter((g: Guest) => g.id !== id));
-              }
-            }}
-          >
-            <Ionicons
-              name="trash-outline"
-              size={24}
-              color={colors.textSecondary}
+    <Link
+      href={{
+        pathname: '/guests/[guestId]',
+        params: { guestId: id },
+      }}
+      asChild
+    >
+      <Pressable>
+        <View style={styles.card}>
+          <View style={styles.avatar}>
+            <Image
+              style={styles.avatarImage}
+              source={{
+                uri: `https://res.cloudinary.com/trueque-image/image/upload/v1713269496/guest-${id}.webp`,
+              }}
+              placeholder={placeholder}
+              placeholderContentFit="cover"
             />
-          </TouchableOpacity>
+          </View>
+          <View style={styles.info}>
+            <Text style={styles.name} numberOfLines={1} ellipsizeMode="tail">
+              {firstName} {lastName}
+            </Text>
+            <Text style={styles.attending}>
+              {attending ? 'Attending' : 'Not Attending'}
+            </Text>
+          </View>
+          <View style={styles.actionWrapper}>
+            <Switch
+              value={attending}
+              onValueChange={async () => {
+                const response = await fetch(`/api/${id}`, {
+                  method: 'PUT',
+                  body: JSON.stringify({
+                    firstName,
+                    lastName,
+                    attending: !attending,
+                  }),
+                });
+
+                if (response.ok) {
+                  setGuests(
+                    guests.map((g) =>
+                      g.id === id ? { ...g, attending: !attending } : g,
+                    ),
+                  );
+                }
+              }}
+              trackColor={{ false: colors.textSecondary, true: colors.switch }}
+              thumbColor={colors.text}
+            />
+            <TouchableOpacity
+              style={styles.button}
+              onPress={async () => {
+                const response = await fetch(`/api/${id}`, {
+                  method: 'DELETE',
+                });
+
+                if (response.ok) {
+                  setGuests(guests.filter((g: Guest) => g.id !== id));
+                }
+              }}
+            >
+              <Ionicons
+                name="trash-outline"
+                size={24}
+                color={colors.textSecondary}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      </Pressable>
+    </Link>
   );
 }
