176353cde8062cc7373f0e45d641f4f7c18d5939
Lukas Prochazka <prochazka.lu@gmail.com>
2024-10-24 15:49:57 +0200

    Fix API order

--- app/api/guests+api.ts
+++ app/api/guests+api.ts
@@ -2,6 +2,52 @@ import { createGuestInsecure, getGuestsInsecure } from '../../database/guests';
 import { ExpoApiResponse } from '../../ExpoApiResponse';
 import { Guest, guestsSchema } from '../../migrations/00000-createTableGuests';
 
+type GuestResponseBodyPost =
+  | {
+      guest: Guest;
+    }
+  | { error: string; errorIssues: { message: string }[] }
+  | { error: string };
+
+export async function POST(
+  request: Request,
+): Promise<ExpoApiResponse<GuestResponseBodyPost>> {
+  const requestBody = await request.json();
+
+  const result = guestsSchema.safeParse(requestBody);
+
+  if (!result.success) {
+    return ExpoApiResponse.json(
+      {
+        error: 'Request does not contain guest object',
+        errorIssues: result.error.issues,
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const newGuest = {
+    firstName: result.data.firstName,
+    lastName: result.data.lastName,
+    attending: false,
+  };
+
+  const guest = await createGuestInsecure(newGuest);
+
+  if (!guest) {
+    return ExpoApiResponse.json(
+      { error: 'Guest not created' },
+      {
+        status: 500,
+      },
+    );
+  }
+
+  return ExpoApiResponse.json({ guest: guest });
+}
+
 type GuestResponseBodyGet = {
   guests: Guest[];
 };
@@ -22,50 +68,4 @@ export async function GET(
       },
     },
   );
-}
-
-type GuestResponseBodyPost =
-  | {
-      guest: Guest;
-    }
-  | { error: string; errorIssues: { message: string }[] }
-  | { error: string };
-
-export async function POST(
-  request: Request,
-): Promise<ExpoApiResponse<GuestResponseBodyPost>> {
-  const requestBody = await request.json();
-
-  const newGuest = {
-    firstName: requestBody.firstName,
-    lastName: requestBody.lastName,
-    attending: false,
-  };
-
-  const result = guestsSchema.safeParse(newGuest);
-
-  if (!result.success) {
-    return ExpoApiResponse.json(
-      {
-        error: 'Request does not contain guest object',
-        errorIssues: result.error.issues,
-      },
-      {
-        status: 400,
-      },
-    );
-  }
-
-  const guest = await createGuestInsecure(newGuest);
-
-  if (!guest) {
-    return ExpoApiResponse.json(
-      { error: 'Guest not created' },
-      {
-        status: 500,
-      },
-    );
-  }
-
-  return ExpoApiResponse.json({ guest: guest });
 }
